cfeacf9f114a739a6bd2456ce7c126ece11c6ddd
main <salimakhond@gmai.com>
2024-05-21 20:59:18 +0600

    interface create

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
-  await mongoose.connect(config.database_url as string);
-
   try {
+    await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
